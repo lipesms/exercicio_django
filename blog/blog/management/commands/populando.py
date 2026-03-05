21e24e6f1318467c6ebacd8f6216c0800709de82
Felipe da Silva Martins <75232625+lipesms@users.noreply.github.com>
2026-03-05 19:53:45 -0300

    editando populando.py

--- blog/blog/management/commands/populando.py
+++ blog/blog/management/commands/populando.py
@@ -1,5 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
 from taxonomy.models import Category, Tag
 
-class command(BaseCommand):
+class Command(BaseCommand):
     help = "A comand to feed the database with a sort of data."
+
+    def handle(self, *args, **options):
+        categoria = Category(name="tecnologia", slug="artigos-de-tecnologia")
+        categoria.save()
+
+# basta mudar o diretorio management para um app especifico
